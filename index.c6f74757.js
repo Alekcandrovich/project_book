2ff0b41ca1659a7c08c17e616866fe776b2b0160
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-05-12 18:51:36 +0000

    Deploying to gh-pages from @ Alekcandrovich/project_book@a195df3e62409dd9005f5027bae39137c968f382 🚀

---
+++ index.c6f74757.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".localStorage").addEventListener("click",onlocal);localStorage.setItem("local",JSON.stringify.idBook)}();
+//# sourceMappingURL=index.c6f74757.js.map
---
+++ index.f2b1048c.js
@@ -0,0 +1,2 @@
+document.querySelector(".localStorage").addEventListener("click",onlocal);localStorage.setItem("local",JSON.stringify.idBook);
+//# sourceMappingURL=index.f2b1048c.js.map
